26ff261a00272454fef866609b3ff7a17f07d589
vjgaur <vijayendra.gaur@gmail.com>
2022-02-02 17:13:14 +0530

    ark etf fund tracking app

---
+++ config.py
@@ -0,0 +1,10 @@
+API_URL = 'https://paper-api.alpaca.markets'
+
+API_KEY = 'PK1LESPDULQ8CIJPHJRH'
+
+API_SECRET = 'aBboio60NRn26E7o1h9FQLWa7aW8w1nkjUvUZBjG'
+
+DB_HOST = 'localhost'
+DB_USER = 'postgres'
+DB_PASS = 'password'
+DB_NAME = 'etfdb'
